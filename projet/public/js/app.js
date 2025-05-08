049362115432ef4c8dedbc3c4df7506cf9d7efb5
Dounia <hajjajidounia8@gmail.com>
2025-05-08 22:24:03 +0100

    Menu

--- projet/public/js/app.js
+++ projet/public/js/app.js
@@ -6,4 +6,28 @@ document.addEventListener('DOMContentLoaded', () => {
       navLinks.classList.toggle('show');
     });
   });
+
+//   Menu
+const filterButtons = document.querySelectorAll(".filter-btn");
+const menuItems = document.querySelectorAll(".menu-item");
+
+filterButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    const category = button.getAttribute("data-category");
+
+    // Active button
+    filterButtons.forEach(btn => btn.classList.remove("active"));
+    button.classList.add("active");
+
+    // Filter logic
+    menuItems.forEach(item => {
+      if (category === "all") {
+        item.style.display = "flex";
+      } else {
+        item.style.display = item.classList.contains(category) ? "flex" : "none";
+      }
+    });
+  });
+});
+
   
