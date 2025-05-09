ec0b119b1f515472969f755ea76be84f631cfc1c
Dounia <hajjajidounia8@gmail.com>
2025-05-09 14:17:57 +0100

    fe

--- projet/public/js/app.js
+++ projet/public/js/app.js
@@ -30,68 +30,34 @@ filterButtons.forEach(button => {
   });
 });
 // & speciales//
-// Liste des données à afficher
 
-let plats = [
-    {
-      titre: "Architecto ut aperiam autem id",
-      sousTitre: "Qui laudantium consequatur laborum sit qui ad sapiente dila parde sonata raqer a videna mareta paulona marka",
-      description: "Et nobis maiores eius. Voluptatibus ut enim blanditiis atque harum sint. Laborum eos ipsum ipsa odit magni. Incidunt hic ut molestiae aut qui. Est repellat minima eveniet eius et quis magni nihil.",
-     
-    },
-    {
-      titre: "Unde praesentium sed",
-      sousTitre: "Qui praesentium velit tempore qui quidem",
-      description: "Voluptatem est necessitatibus vero nihil voluptas vel. Voluptatem omnis temporibus consequatur non doloribus.",
-      image: "./public/images/specials-2.png"
-    },
-    {
-      titre: "Pariatur explicabo vel",
-      sousTitre: "Temporibus quidem rerum voluptates",
-      description: "Odio veritatis perspiciatis quaerat qui aut. Dolorem fuga nam qui. Doloremque voluptas totam dolorem atque.",
-      image: "./public/images/specials-2.png"
-    },
-    {
-      titre: "Nostrum qui quasi",
-      sousTitre: "Et consequatur nihil et explicabo",
-      description: "Consectetur dolore illum quod. Explicabo deleniti ut eveniet. Qui eaque rerum quod.",
-      image: "./public/images/specials-2.png"
-    },
-    {
-      titre: "Iusto ut expedita aut",
-      sousTitre: "Aut deserunt et doloremque velit",
-      description: "Possimus soluta ut id suscipit ea ut in quo quia et soluta. Enim sit et deleniti aut fugiat. Doloribus aut vel autem.",
-      image: "./public/images/specials-2.png"
-    }
-  ];
-  
-  
-  let menu = document.querySelectorAll(".specials-menu li");
-  let titreElement = document.getElementById("special-title");
-  let sousTitreElement = document.getElementById("special-subtitle");
-  let descriptionElement = document.getElementById("special-desc");
-  let imageElement = document.getElementById("special-img");
-  
-  for (let i = 0; i < menu.length; i++) {
-    menu[i].addEventListener("click", function () {
-     
-      for (let j = 0; j < menu.length; j++) {
-        menu[j].classList.remove("active");
+
+
+const menuItem = document.querySelectorAll(".specials-menu li");
+
+const specialItems = document.querySelectorAll(".special-item");
+
+
+menuItem.forEach(item => {
+
+  item.addEventListener("click", () => {
+
+    menuItem.forEach(i => i.classList.remove("active"));
+
+    item.classList.add("active");
+
+    const selectedId = item.getAttribute("data-id");
+
+    specialItems.forEach((special, index) => {
+      if (index == selectedId) {
+        special.style.display = "flex"; 
+      } else {
+        special.style.display = "none"; 
       }
-  
-     
-      this.classList.add("active");
-  
-     
-      let index = this.getAttribute("data-id");
-  
-      
-      titreElement.textContent = plats[index].titre;
-      sousTitreElement.textContent = plats[index].sousTitre;
-      descriptionElement.textContent = plats[index].description;
-      imageElement.setAttribute("src", plats[index].image);
     });
-  }
+  });
+});
+
 //   & reservation
 
   let form = document.querySelector(".php-email-form");
